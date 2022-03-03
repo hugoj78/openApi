6b82d1921795faaae22aac044f37868656b0fd16
hugoj78 <hugoj.egu@gmail.com>
2022-03-03 16:01:06 +0100

    add init

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import requests
 
 app = FastAPI()
 
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Optional[str] = None):
-#     return {"item_id": item_id, "q": q}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
 
 @app.get("/museum")
 def read_museum():
